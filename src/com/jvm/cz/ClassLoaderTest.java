aa9ec74bfbed00c301a5faa00f2956028201731b
root <liuameng521@163.com>
2020-06-22 21:43:41 +0800

    类加载器分类

---
+++ src/com/jvm/cz/ClassLoaderTest.java
@@ -0,0 +1,24 @@
+package com.jvm.cz;
+
+public class ClassLoaderTest {
+
+    public static void main(String[] args) {
+        // 获取系统类加载器
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println(systemClassLoader);//sun.misc.Launcher$AppClassLoader@14dad5dc
+        // 获取其上层，扩展类加载器
+        ClassLoader extClassLoader = systemClassLoader.getParent();
+        System.out.println(extClassLoader);//sun.misc.Launcher$ExtClassLoader@28d93b30
+        // 获取其上层，引导类加载器
+        ClassLoader bootstrapClassLoader = extClassLoader.getParent();
+        System.out.println(bootstrapClassLoader);//null
+        // 对于用户自定义类，默认使用系统类加载器进行加载
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        System.out.println(classLoader);//sun.misc.Launcher$AppClassLoader@14dad5dc
+        // java核心类库都是使用引导类加载器进行加载的
+        ClassLoader classLoader1 = String.class.getClassLoader();
+        System.out.println(classLoader1);//null
+
+
+    }
+}
